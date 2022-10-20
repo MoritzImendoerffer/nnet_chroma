2ab9d5e7f1b796acd6257ba173caab6540c80bad
MoritzImendoerffer <moritz.imend@gmail.com>
2022-10-20 21:13:02 +0200

    started hacking

--- step1_kiss/hack2tegether_v0.1.py
+++ step1_kiss/hack2tegether_v0.1.py
@@ -7,25 +7,183 @@ import torch.autograd as autograd #build a computational graph
 import torch.nn as nn ## neural net library
 import torch.nn.functional as F ## most non-linearities are here
 import torch.optim as optim
+import scipy
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
-class NN(nn.Module):
 
-    def __int__(self):
-        super().__init__()
-        self.flatten = nn.Flatten()
-        self.nn_simple = nn.Sequential(
-            nn.Linear(28*28, 512),
-            nn.ReLU(),
-            nn.Linear(512, 512),
-            nn.ReLU(),
-            nn.Linear(512, 10)
+# the deep neural network
+class DNN(torch.nn.Module):
+    def __init__(self, layers):
+        super(DNN, self).__init__()
 
-        )
+        layer_list = list()
+        layer_dict = dict()
+        for i in range(len(layers) - 2):
+            lname = f"layer_{i:d}"
+            layer_dict[lname] = torch.nn.Linear(layers[i], layers[i + 1])
+
+            aname = f"activation_{i:d}"
+            layer_dict[aname] = torch.nn.Tanh()
+
+        lname = f"layer_{len(layers) - 2: d}"
+        layer_dict[lname] = torch.nn.Linear(layers[-2], layers[-1])
+
+        # deploy layers
+        self.layers = torch.nn.Sequential(layer_dict)
 
         def forward(self, x):
-            x = self.flatten(x)
-            logits = self.nn_simple(x)
-            return logits
-model = NN().to(device)
+            out = self.layers(x)
+            return out
+
+
+# the physics-guided neural network
+class PhysicsInformedNN():
+    def __init__(self, X, u, layers, lb, ub):
+
+        # boundary conditions
+        self.lb = torch.tensor(lb).float().to(device)
+        self.ub = torch.tensor(ub).float().to(device)
+
+        # data
+        self.x = torch.tensor(X[:, 0:1], requires_grad=True).float().to(device)
+        self.t = torch.tensor(X[:, 1:2], requires_grad=True).float().to(device)
+        self.u = torch.tensor(u).float().to(device)
+
+        # settings
+        self.lambda_1 = torch.tensor([0.0], requires_grad=True).to(device)
+        self.lambda_2 = torch.tensor([-6.0], requires_grad=True).to(device)
+
+        self.lambda_1 = torch.nn.Parameter(self.lambda_1)
+        self.lambda_2 = torch.nn.Parameter(self.lambda_2)
+
+        # deep neural networks
+        self.dnn = DNN(layers).to(device)
+        self.dnn.register_parameter('lambda_1', self.lambda_1)
+        self.dnn.register_parameter('lambda_2', self.lambda_2)
+
+        # optimizers: using the same settings
+        self.optimizer = torch.optim.LBFGS(
+            self.dnn.parameters(),
+            lr=1.0,
+            max_iter=50000,
+            max_eval=50000,
+            history_size=50,
+            tolerance_grad=1e-5,
+            tolerance_change=1.0 * np.finfo(float).eps,
+            line_search_fn="strong_wolfe"  # can be "strong_wolfe"
+        )
+
+        self.optimizer_Adam = torch.optim.Adam(self.dnn.parameters())
+        self.iter = 0
+
+    def net_u(self, x, t):
+        u = self.dnn(torch.cat([x, t], dim=1))
+        return u
+
+    def net_f(self, x, t):
+        """ The pytorch autograd version of calculating residual """
+        lambda_1 = self.lambda_1
+        lambda_2 = torch.exp(self.lambda_2)
+        u = self.net_u(x, t)
+
+        u_t = torch.autograd.grad(
+            u, t,
+            grad_outputs=torch.ones_like(u),
+            retain_graph=True,
+            create_graph=True
+        )[0]
+        u_x = torch.autograd.grad(
+            u, x,
+            grad_outputs=torch.ones_like(u),
+            retain_graph=True,
+            create_graph=True
+        )[0]
+        u_xx = torch.autograd.grad(
+            u_x, x,
+            grad_outputs=torch.ones_like(u_x),
+            retain_graph=True,
+            create_graph=True
+        )[0]
+
+        f = u_t + lambda_1 * u * u_x - lambda_2 * u_xx
+        return f
+
+    def loss_func(self):
+        u_pred = self.net_u(self.x, self.t)
+        f_pred = self.net_f(self.x, self.t)
+        loss = torch.mean((self.u - u_pred) ** 2) + torch.mean(f_pred ** 2)
+        self.optimizer.zero_grad()
+        loss.backward()
+
+        self.iter += 1
+        if self.iter % 100 == 0:
+            print(
+                'Loss: %e, l1: %.5f, l2: %.5f' %
+                (
+                    loss.item(),
+                    self.lambda_1.item(),
+                    torch.exp(self.lambda_2.detach()).item()
+                )
+            )
+        return loss
+
+    def train(self, nIter):
+        self.dnn.train()
+        for epoch in range(nIter):
+            u_pred = self.net_u(self.x, self.t)
+            f_pred = self.net_f(self.x, self.t)
+            loss = torch.mean((self.u - u_pred) ** 2) + torch.mean(f_pred ** 2)
+
+            # Backward and optimize
+            self.optimizer_Adam.zero_grad()
+            loss.backward()
+            self.optimizer_Adam.step()
+
+            if epoch % 100 == 0:
+                print(
+                    'It: %d, Loss: %.3e, Lambda_1: %.3f, Lambda_2: %.6f' %
+                    (
+                        epoch,
+                        loss.item(),
+                        self.lambda_1.item(),
+                        torch.exp(self.lambda_2).item()
+                    )
+                )
+
+        # Backward and optimize
+        self.optimizer.step(self.loss_func)
+
+    def predict(self, X):
+        x = torch.tensor(X[:, 0:1], requires_grad=True).float().to(device)
+        t = torch.tensor(X[:, 1:2], requires_grad=True).float().to(device)
+
+        self.dnn.eval()
+        u = self.net_u(x, t)
+        f = self.net_f(x, t)
+        u = u.detach().cpu().numpy()
+        f = f.detach().cpu().numpy()
+        return u, f
+
+nu = 0.01/np.pi
+
+N_u = 2000
+layers = [2, 20, 20, 20, 20, 20, 20, 20, 20, 1]
+
+data = scipy.io.loadmat('data/burgers_shock.mat')
+
+t = data['t'].flatten()[:,None]
+x = data['x'].flatten()[:,None]
+Exact = np.real(data['usol']).T
+
+X, T = np.meshgrid(x,t)
+
+X_star = np.hstack((X.flatten()[:,None], T.flatten()[:,None]))
+u_star = Exact.flatten()[:,None]
+
+# Doman bounds
+lb = X_star.min(0)
+ub = X_star.max(0)
+
+
+
